66eacf6e9be6d2659ad44f2ab38f5a11201135bd
konka <1784konka@gmail.com>
2021-09-13 13:46:35 +0600

    commit2

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      title: 'CRUD Application with React!' ,
+    this.state = {
+      title: 'CRUD Application with React!',
       act: 0,
       index: '',
       datas: []
     }
-  } 
+  }
 
-  componentDidMount(){
+  componentDidMount() {
     this.refs.name.focus();
   }
 
-  fSubmit = (e) =>{
+  fSubmit = (e) => {
     e.preventDefault();
     console.log('try');
 
@@ -27,17 +27,17 @@ class App extends Component {
     let ToDo = this.refs.ToDo.value;
 
 
-    if(this.state.act === 0){   //new
+    if (this.state.act === 0) {   //new
       let data = {
-        name, mail,ToDo
+        name, mail, ToDo
       }
       datas.push(data);
-    }else{                      //update
+    } else {                      //update
       let index = this.state.index;
       datas[index].name = name;
       datas[index].mail = mail;
       datas[index].ToDo = ToDo;
-    }    
+    }
 
     this.setState({
       datas: datas,
@@ -50,7 +50,7 @@ class App extends Component {
 
   fRemove = (i) => {
     let datas = this.state.datas;
-    datas.splice(i,1);
+    datas.splice(i, 1);
     this.setState({
       datas: datas
     });
@@ -72,7 +72,7 @@ class App extends Component {
     });
 
     this.refs.name.focus();
-  }  
+  }
 
   render() {
     let datas = this.state.datas;
@@ -82,20 +82,26 @@ class App extends Component {
         <form ref="myForm" className="myForm">
           <input type="text" ref="name" placeholder="Name" className="formField" />
           <input type="text" ref="mail" placeholder="E-mail" className="formField" />
-          <input type="text" ref="ToDo" placeholder="Task" className="formField" />
-          <button onClick={(e)=>this.fSubmit(e)} className="myButton">submit </button>
+          <input type="text" ref="ToDo" placeholder="TO-D0" className="formField" />
+          <button onClick={(e) => this.fSubmit(e)} className="myButton">submit </button>
         </form>
         <pre>
           {datas.map((data, i) =>
-            <li key={i} className="myList">
-              {i+1}. {data.name}, {data.mail},{data.ToDo}
-              <button onClick={()=>this.fRemove(i)} className="myListButton"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-  <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-</svg></button>
-              <button onClick={()=>this.fEdit(i)} className="myListButton"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-</svg> </button>
-            </li>
+            <div key={i} className="myList" >
+              <div style={{ display: 'flex', flexFlow: "row", justifyContent: "space-between" }}>
+                <div class="user" >
+                <b> User{i +1}</b><br/>Name:{data.name}<br/>Email:{data.mail}<br/>Task:{data.ToDo}
+                </div>
+                <div className="myListButtonDesign" style={{float : 'right'}}>
+                  <button onClick={() => this.fRemove(i)} className="myListButton"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                  </svg></button>
+                  <button onClick={() => this.fEdit(i)} className="myListButton"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                  </svg> </button>
+                </div>
+              </div>
+            </div>
           )}
         </pre>
       </div>
